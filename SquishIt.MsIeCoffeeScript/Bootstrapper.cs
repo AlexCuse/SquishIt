6b9786619b692e3150b343bfef8e86e24ffb6df3
Alex Ullrich <alex.ullrich@gmail.com>
2013-03-06 20:30:56 -0500

    initialize MsIeCoffeescript via PreApplicationStartMethod (#232)

---
+++ SquishIt.MsIeCoffeeScript/Bootstrapper.cs
@@ -0,0 +1,12 @@
+﻿using SquishIt.Framework;
+
+namespace SquishIt.MsIeCoffeeScript
+{
+    public class Bootstrapper
+    {
+        public static void Initialize()
+        {
+            Bundle.RegisterScriptPreprocessor(new CoffeeScriptPreprocessor());
+        }
+    }
+}
