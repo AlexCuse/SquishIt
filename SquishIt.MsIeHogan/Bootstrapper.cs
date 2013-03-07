4c45bf6484ff836a407c735b6837d3ad7a320b60
Alex Ullrich <alex.ullrich@gmail.com>
2013-03-06 20:33:16 -0500

    initialize MsIeHogan via PreApplicationStartMethod (#232)

---
+++ SquishIt.MsIeHogan/Bootstrapper.cs
@@ -0,0 +1,12 @@
+﻿using SquishIt.Framework;
+
+namespace SquishIt.MsIeHogan
+{
+    public class Bootstrapper
+    {
+        public static void Initialize()
+        {
+            Bundle.RegisterScriptPreprocessor(new HoganPreprocessor());
+        }
+    }
+}
