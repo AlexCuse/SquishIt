15355456ba5f02bc2b6e797519f2caecd4b565c0
Alex Ullrich <alex.ullrich@gmail.com>
2013-03-06 20:21:56 -0500

    initialize SASS via PreApplicationStartMethod (#232)

---
+++ SquishIt.Sass/Bootstrapper.cs
@@ -0,0 +1,12 @@
+﻿using SquishIt.Framework;
+
+namespace SquishIt.Sass
+{
+    public class Bootstrapper
+    {
+        public static void Initialize()
+        {
+            Bundle.RegisterStylePreprocessor(new SassPreprocessor());
+        }
+    }
+}
