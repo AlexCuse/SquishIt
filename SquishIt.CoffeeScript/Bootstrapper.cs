573af9be0f7f0f680588a398c5104c263ead44e3
Alex Ullrich <alex.ullrich@gmail.com>
2013-03-06 20:27:33 -0500

    initialize Coffeescript via PreApplicationStartMethod (#232)

---
+++ SquishIt.CoffeeScript/Bootstrapper.cs
@@ -0,0 +1,12 @@
+﻿using SquishIt.Framework;
+
+namespace SquishIt.CoffeeScript
+{
+    public class Bootstrapper
+    {
+        public static void Initialize()
+        {
+            Bundle.RegisterScriptPreprocessor(new CoffeeScriptPreprocessor());
+        }
+    }
+}
--- SquishItAspNetMvcTest/Global.asax.cs
+++ SquishItAspNetMvcTest/Global.asax.cs
@@ -1,15 +1,6 @@
 ﻿using System.Web.Mvc;
 using System.Web.Routing;
 using SquishIt.Framework;
-/*
-//uncomment this (and comment out MsIe equivalents) to test jurassic-based preprocessors
-using SquishIt.CoffeeScript;
-using SquishIt.Hogan;
-*/
-using SquishIt.MsIeCoffeeScript;
-using SquishIt.MsIeHogan;
-using SquishIt.Less;
-using SquishIt.Sass;
 
 namespace SquishItAspNetMvcTest
 {
@@ -41,11 +32,6 @@ namespace SquishItAspNetMvcTest
 
             RegisterGlobalFilters(GlobalFilters.Filters);
             RegisterRoutes(RouteTable.Routes);
-            //Bundle.RegisterScriptPreprocessor(new HoganPreprocessor());
-            Bundle.RegisterScriptPreprocessor(new CoffeeScriptPreprocessor());
-
-            //Bundle.RegisterStylePreprocessor(new LessPreprocessor());
-            //Bundle.RegisterStylePreprocessor(new SassPreprocessor());
 
             Bundle.JavaScript()
                 .Add("/assets/js/jquery_1.7.2.js")
